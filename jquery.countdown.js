c4678df3fdcdd6f8e9923b713004855313d8362d
hunnnnny-v <96787839+hunnnnny-v@users.noreply.github.com>
2022-05-15 14:42:24 +0530

    Update jquery.countdown.js

--- jquery.countdown.js
+++ jquery.countdown.js
@@ -1,7 +1,7 @@
 /*!
  * The Final Countdown for jQuery v2.2.0 (http://hilios.github.io/jQuery.countdown/)
  * Copyright (c) 2016 Edson Hilios
- * 
+ * These are comments used in order to understand the website.
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
